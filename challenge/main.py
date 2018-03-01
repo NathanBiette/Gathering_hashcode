af9151d5c053836ab4330eaca81f82199c3d211f
Nathan Biette <nathan1.biette@free.fr>
2018-03-01 21:56:48 +0100

    Update completed rides

--- challenge/main.py
+++ challenge/main.py
@@ -26,7 +26,7 @@ for vehicle_number in range(info['vehicles']):
     best_ride = find_best_ride(vehicle_number, available_rides)
     ride_assignment += [[best_ride]]
     timer += [setTimer(vehicle_number, best_ride, 0, ride_assignment, available_rides)]
-    update_available_rides(available_rides, best_ride)
+    available_rides[best_ride]['is_complete'] = True
 
 #boucle sur le temps (chaque étapes)
 for time in range(info['steps']):
@@ -39,6 +39,7 @@ for time in range(info['steps']):
         best_ride = find_best_ride(vehicle_number, available_rides)
         #ajoute le meilleur ride trouvé
         ride_assignment[vehicle_number] += [best_ride]
+        available_rides[best_ride]['is_complete'] = True
         #reset le temps
         timer[vehicle_number] = ride_time(best_ride)
 
@@ -50,7 +51,7 @@ for time in range(info['steps']):
 total_steps = 1000
 constante = math.log(2)
 #le score de l'assignment d'origine (mettre les arguments)
-score = total_score()
+score = totalScore()
 
 def proba_selection(actual_step):
     return 1 - math.exp((-1)*constante/actual_step)
