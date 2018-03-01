755c479f845501dd81b532d18334decac1784a5b
aurelien.lebray@gmail.com <aurelien.lebray@gmail.com>
2018-03-01 19:51:57 +0100

    parsing

--- challenge/gathering_parser.py
+++ challenge/gathering_parser.py
@@ -1,5 +1,29 @@
 class GatheringParser:
 	def parse(self, input_file):
 		file = open(input_file, 'r')
-		print(file.read())
+		
+		# Get datas
+		lines = file.readlines()
+		first_datas = lines[0].split()
+		rides = []
+		for line in lines[1:]:
+			line_datas = line.split()
+			infos = {
+				'pos_start': (int(line_datas[0]), int(line_datas[1])),
+				'pos_finish': (int(line_datas[2]), int(line_datas[3])),
+				'start': int(line_datas[4]),
+				'finish': int(line_datas[5])
+			}
+			rides.append(infos)
+
 		file.close()
+
+		return ({
+			'rows': int(first_datas[0]),
+			'columns': int(first_datas[1]),
+			'vehicles': int(first_datas[2]),
+			'nb_rides': int(first_datas[3]),
+			'bonus': int(first_datas[4]),
+			'steps': int(first_datas[5]),
+			'rides': rides
+			})
--- challenge/main.py
+++ challenge/main.py
@@ -3,6 +3,6 @@ import sys
 
 parser = Parser()
 if len(sys.argv) == 2:
-	parser.parse(sys.argv[1])
+	print(parser.parse(sys.argv[1]))
 else:
 	print("Syntax error:\r\nUsage : python main.py file_name")
