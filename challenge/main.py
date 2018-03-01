641ee780ba980b089ce76e7b5a403f69b29c0078
aurelien.lebray@gmail.com <aurelien.lebray@gmail.com>
2018-03-01 21:31:16 +0100

    modified main and added submission

--- challenge/main.py
+++ challenge/main.py
@@ -8,7 +8,7 @@ if len(sys.argv) == 2:
 	parser.parse(sys.argv[1])
 else:
 	print("Syntax error:\r\nUsage : python main.py file_name")
-splited_path = re.split('/|\\|.', sys.argv[1])
-print(splited_path)
+	exit()
+splited_path = re.split('/|\.', sys.argv[1])
 file_name = splited_path[len(splited_path) - 2] # just before the .in extension
-got([[0, 3, 4], [1, 2, 5]], file_name)
+got([[0, 3, 4], [1, 2]], file_name)
