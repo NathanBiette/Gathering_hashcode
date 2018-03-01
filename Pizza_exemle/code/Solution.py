1f74b1ca41cbf61c41b43ba563e6ef16f83859a5
Nathan Biette <nathan1.biette@free.fr>
2018-03-01 18:20:55 +0100

    Add pizza parser

--- Pizza_exemle/code/Solution.py
+++ Pizza_exemle/code/Solution.py
@@ -8,3 +8,18 @@ C = int(numbers[1])
 L = int(numbers[2])
 H = int(numbers[3])
 print(R, C, L, H)
+
+def parse_pizza(data):
+    pizza = []
+    for index, line in enumerate(data[1:]):
+        row = []
+        for char in line:
+            if char == 'T':
+                row += [0]
+            if char == 'M':
+                row += [1]
+        pizza += [row]
+    return pizza
+
+
+print(parse_pizza(data))
